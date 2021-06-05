5bd857867558d5cd5536d69a9185ea0bd9c1cd86
deaviss <deaviss@gmail.com>
2021-06-05 23:56:40 +0200

    update1

--- src/Pages/SignInPage.js
+++ src/Pages/SignInPage.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import "./SignIn.css"
-import {Link} from "react-router-dom"
+import "./SignInPage.css"
+import { Link } from "react-router-dom"
+import { Button, FormControl, TextField } from '@material-ui/core'
 
 export default function SignInPage() {
 
 
+	return (
+		<div className="SignIn">
+			<div className="SignIn__box">
+				<h1 className="SignIn__h1">Sign in</h1>
+				<form className="SignIn__form" autoComplete="off">
+					<TextField required inputProps={{ minLength: 8 }} id="login" label="Login" variant="outlined" fullWidth="100%" />
+					<TextField required inputProps={{ minLength: 8 }} style={{ marginTop: "20px" }} id="password" label="Password" type="password" variant="outlined" fullWidth="100%" />
 
-    return (
-        <div className="SignIn">
-            <div className="SignIn__box">
-                <h1 className="SignIn__h1">Sign in</h1>
-                {/* input:login */}
-                {/* input:password */}
-                <Link to="#">Forgot password?</Link>
-                <Link to="#">Sign up</Link>
-            </div>
+					<Link className="SignIn__link" to="#">Forgot password?</Link>
+					<Link className="SignIn__link" to="#">Sign up</Link>
+					<div className="SignIn__button_wrap">
+						<Button type="submit" style={{ marginTop: "20px" }} variant="contained" size="large" color="primary">
+							Sign in
+						</Button>
+					</div>
+				</form>
+			</div>
 
-        </div>
-    )
+		</div>
+	)
 }
