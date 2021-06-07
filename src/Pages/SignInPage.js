107373b905a6eb2a8b5b1d87ed99077f9d23e9ce
Berat Kales <bkales123@gmail.com>
2021-06-07 02:42:43 -0400

    Created component for submit buttons

--- src/Pages/SignInPage.js
+++ src/Pages/SignInPage.js
@@ -4,16 +4,17 @@ import {Link} from "react-router-dom"
 
 export default function SignInPage() {
 
+    
 
 
     return (
         <div className="SignIn">
             <div className="SignIn__box">
-                <h1 className="SignIn__h1">Sign in</h1>
+                <h1 className="SignIn__h1">Sign In</h1>
                 {/* input:login */}
                 {/* input:password */}
-                <Link to="#">Forgot password?</Link>
-                <Link to="#">Sign up</Link>
+                <Link className="SignIn__link" to="#">Forgot password?</Link>
+                <Link className="SignIn__link" to="#">Sign up</Link>
             </div>
 
         </div>
